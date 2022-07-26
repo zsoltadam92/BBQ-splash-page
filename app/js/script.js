2211fac2fe849368ab1455227131f41ea0d66e0d
zsoltadam92 <zsoltadam92@gmail.com>
2022-07-26 16:50:07 +0200

    localstorage,regex

--- app/js/script.js
+++ app/js/script.js
@@ -1,11 +1,16 @@
 console.log('hello');
 
+
+const form = document.querySelector('.main__form')
 const firstName = document.getElementById('name');
 const errorLength = document.querySelector('.main__error--length')
 const errorUppercase = document.querySelector('.main__error--uppercase')
 const email = document.getElementById('email');
 const errorEmail = document.querySelector('.main__error--email')
 const submit = document.getElementById('submit');
+
+firstName.value = localStorage.getItem('firstName');
+email.value = localStorage.getItem('email');
 
 firstName.addEventListener('input' , () => {
   let text = firstName.value;
@@ -27,14 +32,19 @@ firstName.addEventListener('input' , () => {
 
 email.addEventListener('input' , () => {
   let text = email.value;
-  let pattern = /[\wáéíóöőúüűÁÉÍÓÖŐÚÜŰ]{6,}@[a-zA-Z]{4,}.[a-zA-Z]{2,3}$/;
+  let pattern = /[\wáéíóöőúüűÁÉÍÓÖŐÚÜŰ.-]{6,}@[a-zA-Z]{4,}.[a-zA-Z]{2,3}$/;
   let result = pattern.test(text);
 
-console.log(result)
   if(!result) {
     errorEmail.style.display = "block"; 
   } else {
     errorEmail.style.display = "none"; 
   }
+})
 
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('firstName', firstName.value)
+  localStorage.setItem('email', email.value)
+  form.reset()
 })
